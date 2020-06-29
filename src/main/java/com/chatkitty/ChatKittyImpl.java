e10d1e905ab919910bc2ce2100e35ab3f39d61c9
Alex Nguyen <alexnguyen@live.ca>
2020-06-29 18:08:56 -0400

    feat: Add StompX implementation

--- src/main/java/com/chatkitty/ChatKittyImpl.java
+++ src/main/java/com/chatkitty/ChatKittyImpl.java
@@ -15,16 +15,26 @@
  */
 package com.chatkitty;
 
-import com.chatkitty.stompx.stompx.StompWebSocketClient;
-import com.chatkitty.stompx.stompx.WebSocketConfiguration;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jetbrains.annotations.Nullable;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.chatkitty.model.session.SessionStartResult;
+import com.chatkitty.stompx.stompx.StompWebSocketClient;
+import com.chatkitty.stompx.stompx.StompWebSocketClientCallBack;
+import com.chatkitty.stompx.stompx.WebSocketConfiguration;
+import com.chatkitty.stompx.stompx.stomp.StompSubscription;
+import com.chatkitty.stompx.stompx.stomp.WebSocketEvent;
+import com.chatkitty.stompx.stompx.stomp.stompframe.StompServerFrame;
+
 import okhttp3.OkHttpClient;
 
 public class ChatKittyImpl implements ChatKitty {
+
   private final String apiKey;
-  @Nullable
-  private StompWebSocketClient client;
+  @Nullable private StompWebSocketClient client;
 
   public ChatKittyImpl(String apiKey) {
     this.apiKey = apiKey;
@@ -32,11 +42,49 @@ public class ChatKittyImpl implements ChatKitty {
 
   @Override
   public void startSession(String username, ChatKittyCallback callback) {
-    WebSocketConfiguration configuration = new WebSocketConfiguration(apiKey,
-        username, "https://staging-api.chatkitty.com", "/stompx");
+    WebSocketConfiguration configuration =
+        new WebSocketConfiguration(
+            apiKey, username, "https://staging-api.chatkitty.com", "/stompx");
 
     client = new StompWebSocketClient(new OkHttpClient(), new ObjectMapper(), configuration);
-    // TODO - Subscribe to client, when object receive use the ChatKittyCallback.
     client.start();
+
+    client.subscribeRelay(
+        "/application/users.me.relay",
+        new WebSocketClientCallBack<SessionStartResult>(SessionStartResult.class) {
+          @Override
+          void onParsedMessage(
+              SessionStartResult resource,
+              StompWebSocketClient client,
+              StompSubscription subscription) {
+            callback.onSuccess(resource);
+          }
+        });
+  }
+
+  private abstract static class WebSocketClientCallBack<T> implements StompWebSocketClientCallBack {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final Class<T> type;
+
+    WebSocketClientCallBack(Class<T> type) {
+      this.type = type;
+    }
+
+    @Override
+    public void onNewMessage(
+        StompWebSocketClient client, StompServerFrame frame, StompSubscription subscription) {
+      try {
+        JavaType javaType =
+            objectMapper.getTypeFactory().constructParametricType(WebSocketEvent.class, type);
+        WebSocketEvent<T> response = objectMapper.readValue(frame.body, javaType);
+        onParsedMessage(response.resource, client, subscription);
+      } catch (JsonProcessingException e) {
+        e.printStackTrace();
+      }
+    }
+
+    abstract void onParsedMessage(
+        T resource, StompWebSocketClient client, StompSubscription subscription);
   }
 }
